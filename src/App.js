527c4b0bf7d4d9e4203c225f5fba6e7d497fb1af
Muhammad Kashif <kashif@thegemcloud.com>
2023-01-02 23:28:41 +0500

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
+// css files
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './img/icons/css/ionicons.css';
+import './img/font-awesome/css/font-awesome.css';
+import './style.css';
+
+import NavBar from './components/navbar';
+import Intro from './components/intro';
+import About from './components/about';
+import Portfolio from './components/portfolio';
+import Contact from './components/contact';
+import BackToTop from './components/back-to-top';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +30,14 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <NavBar />
+      <Intro />
+      <About />
+      <Portfolio />
+      <Contact />
+      <BackToTop />
+      
     </div>
   );
 }
